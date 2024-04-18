ff497bde911805884eb9ff6c6ea924558342ed4b
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-04-18 22:25:23 +0600

    environmet key added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAFM4HaMtE5V_GRv6kBpkBG8e_jdoXnNyQ",
-  authDomain: "stay-haven.firebaseapp.com",
-  projectId: "stay-haven",
-  storageBucket: "stay-haven.appspot.com",
-  messagingSenderId: "558936539528",
-  appId: "1:558936539528:web:5e7967cfe5dbebd7897a3d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
