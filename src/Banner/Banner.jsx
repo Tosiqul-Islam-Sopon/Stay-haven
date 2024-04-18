71eb418c2431f67d9f143f44120d86a39c4109a5
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-04-18 21:05:23 +0600

    swiper added

---
+++ src/Banner/Banner.jsx
@@ -0,0 +1,72 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import "./Banner.css"
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+import slide_image_1 from "../../src/assets/images/1.jpg"
+import slide_image_2 from "../../src/assets/images/2.jpg"
+import slide_image_3 from "../../src/assets/images/3.jpg"
+import slide_image_4 from "../../src/assets/images/4.jpg"
+
+
+function Banner() {
+    const bannerHeading = <>
+        <div className=" text-white">
+            <p className="text-white text-3xl lg:text-7xl font-bold">STAY <span className="text-[#cf827c]">HAVEN</span></p>
+            <p className="text-white text-xl lg:text-3xl font-semibold text-center">Stay with us, feel Haven</p>
+        </div>
+    </>
+
+    return (
+        <div className=''>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="w-full text-center py-32 md:py-56 flex flex-col bg-no-repeat bg-cover bg-center " style={{ backgroundImage: `url(${slide_image_1})` }}>
+                        {
+                            bannerHeading
+                        }
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="w-full text-center py-32 md:py-56 flex flex-col bg-no-repeat bg-cover bg-center " style={{ backgroundImage: `url(${slide_image_2})` }}>
+                        {
+                            bannerHeading
+                        }
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className=" w-full text-center py-32 md:py-56 flex flex-col bg-no-repeat bg-cover bg-center " style={{ backgroundImage: `url(${slide_image_3})` }}>
+                        {
+                            bannerHeading
+                        }
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="w-full text-center py-32 md:py-56 flex flex-col bg-no-repeat bg-cover bg-center " style={{ backgroundImage: `url(${slide_image_4})` }}>
+                        {
+                            bannerHeading
+                        }
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+    );
+}
+
+export default Banner;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,6 +9,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 import 'animate.css';
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     DocumentTitle("Home");
@@ -19,52 +20,7 @@ const Home = () => {
       }, [])
     return (
         <div>
-            <div data-aos="fade-left" className="animate__animated animate__backInRight carousel w-full max-h-screen ">
-                <div id="slide1" className="carousel-item relative w-full ">
-                    <img src={image1} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <div className="flex flex-col items-center justify-center gap-3">
-                            <p className="text-white text-3xl lg:text-5xl font-bold">STAY <span className="text-[#cf827c]">HAVEN</span></p>
-                            <p className="text-white text-xl lg:text-3xl font-semibold text-center">Stay with us, feel Haven</p>
-                        </div>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src={image2} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <div className="flex flex-col items-center justify-center gap-3">
-                            <p className="text-white text-3xl lg:text-5xl font-bold">STAY <span className="text-[#cf827c]">HAVEN</span></p>
-                            <p className="text-white text-xl lg:text-3xl font-semibold text-center">Stay with us, feel Haven</p>
-                        </div>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src={image3} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <div className="flex flex-col items-center justify-center gap-3">
-                            <p className="text-white text-3xl lg:text-5xl font-bold">STAY <span className="text-[#cf827c]">HAVEN</span></p>
-                            <p className="text-white text-xl lg:text-3xl font-semibold text-center">Stay with us, feel Haven</p>
-                        </div>
-                        <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src={image4} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <div className="flex flex-col items-center justify-center gap-3">
-                            <p className="text-white text-3xl lg:text-5xl font-bold">STAY <span className="text-[#cf827c]">HAVEN</span></p>
-                            <p className="text-white text-xl lg:text-3xl font-semibold text-center">Stay with us, feel Haven</p>
-                        </div>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-            </div>
+            <Banner></Banner>
             <div data-aos="fade-up" className="animate__animated animate__backInUp p-2 lg:p-10 space-y-4 text-center">
                 <h1 className="text-4xl font-bold text-center">Estates</h1>
                 <p>Welcome to STAYHAVEN, where hospitality meets excellence. Discover a diverse selection of accommodations, from luxurious resorts to cozy lodges, tailored to every preference and budget. Your haven away from home awaits at STAYHAVEN, where personalized service and seamless booking ensure an unforgettable experience.</p>
